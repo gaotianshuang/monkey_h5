26b2425f369c993a6d3e622873c01f0ef4569e21
flint <xuejianchao.flint@bytedance.com>
2022-04-01 23:20:13 +0800

    feat: 开发藏品页的兜底界面

--- src/views/collection/pages/Index.vue
+++ src/views/collection/pages/Index.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="wrap">
-    <h1>collection</h1>
+    <Empty />
   </div>
 </template>
+
+<script lang="ts">
+import Empty from '../components/Empty/Index.vue'
+export default {
+  components: {
+    Empty
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.wrap{
+  width: 100%;
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
